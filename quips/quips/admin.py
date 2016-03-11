44e954ca0e711df3f28fa86c073f0ffe5b20fc42
Brad Smith <bradster@infinitewarp.com>
2016-03-10 22:16:08 -0500

    enable quote management within quip admin

--- quips/quips/admin.py
+++ quips/quips/admin.py
@@ -7,10 +7,18 @@ from ordered_model.admin import OrderedModelAdmin
 from quips.quips.models import Quip, Quote, Speaker
 
 
+class QuoteInline(admin.TabularInline):
+    model = Quote
+    extra = 1
+
+
 class QuoteAdmin(OrderedModelAdmin):
     list_display = ('id', 'quip', 'text', 'speaker', 'move_up_down_links')
 
 
+class QuipAdmin(admin.ModelAdmin):
+    inlines = [QuoteInline]
+
 admin.site.register(Quote, QuoteAdmin)
-admin.site.register(Quip)
+admin.site.register(Quip, QuipAdmin)
 admin.site.register(Speaker)
