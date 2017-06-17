3a160f82accd55d9e4a98d932b6e18042803ef5e
Brad Smith <bradster@infinitewarp.com>
2017-06-17 00:48:20 -0400

    better obfuscate hyphenated names

--- quips/quips/templatetags/quips_extras.py
+++ quips/quips/templatetags/quips_extras.py
@@ -9,12 +9,14 @@ register = template.Library()
 
 @register.filter(name='obfuscate_name')
 @memoize(timeout=5)
-def obfuscate_name(value):
-    names = value.split(' ')
+def obfuscate_name(value, delimeter=' '):
+    names = value.split(delimeter)
 
     def shuffle_word(word):
         if not word or len(word) < 3:
             return word
+        if '-' in word:
+            return obfuscate_name(word, delimeter='-')
         guts = list(word[1:-1])
         random.shuffle(guts)
         new_word = word[0] + ''.join(guts) + word[-1]
@@ -23,7 +25,7 @@ def obfuscate_name(value):
             new_word = word[0] + ''.join(guts[::-1]) + word[-1]
         return new_word
 
-    new_name = ' '.join([shuffle_word(word) for word in names])
+    new_name = delimeter.join([shuffle_word(word) for word in names])
     return new_name
 
 
