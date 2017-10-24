ce07542edc6cebdb3705674ac78d1e16cf4f6c96
Brad Smith <bradster@infinitewarp.com>
2017-10-24 17:47:53 -0400

    update giphys

--- quips/quips/templatetags/quips_extras.py
+++ quips/quips/templatetags/quips_extras.py
@@ -47,11 +47,13 @@ def rot13_name(value):
 # TODO Make this configurable/DB-driven?
 GIPHYS = [
     '<iframe src="//giphy.com/embed/LPn77YyDIqfhm?html5=true&hideSocial=true" width="480" height="270" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
-    '<iframe src="//giphy.com/embed/6OWIl75ibpuFO?html5=true&hideSocial=true" width="480" height="360" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
+    '<iframe src="//giphy.com/embed/XsUtdIeJ0MWMo?html5=true&hideSocial=true" width="480" height="360" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
     '<iframe src="//giphy.com/embed/52VjAeGgj78GY?html5=true&hideSocial=true" width="480" height="377" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
     '<iframe src="//giphy.com/embed/b1NpJFw89s7UA?html5=true&hideSocial=true" width="480" height="355" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
-    '<iframe src="//giphy.com/embed/5MuY6NkkbiASY?html5=true&hideSocial=true" width="480" height="363" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
-    '<iframe src="//giphy.com/embed/FRNuP6zwkDhcc?html5=true&hideSocial=true" width="480" height="384" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
+    '<iframe src="//giphy.com/embed/baDHsD0Bk3K5a?html5=true&hideSocial=true" width="480" height="384" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
+    '<iframe src="//giphy.com/embed/xSM46ernAUN3y?html5=true&hideSocial=true" width="480" height="384" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
+    '<iframe src="//giphy.com/embed/oTiG8QJA2Kldm?html5=true&hideSocial=true" width="480" height="384" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
+    '<iframe src="//giphy.com/embed/jhmjR9RZYS92M?html5=true&hideSocial=true" width="480" height="384" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
 ]
 
 
