d32c33124ca7459937b5db4c720c709f0ecbf6ff
Brad Smith <bradster@infinitewarp.com>
2020-08-14 02:51:07 -0400

    add test for anonymize.obfuscate_name

--- quips/quips/tests/test_anonymize.py
+++ quips/quips/tests/test_anonymize.py
@@ -1,28 +1,32 @@
 from django.test import TestCase
 
-import quips.quips.anonymize
+from quips.quips import anonymize
 
 
 class AnonymizeTest(TestCase):
-    def test_shuffle_word_long(self):
-        """Test a reasonably long word is shuffled."""
-        word = "Enterprise"
-        shuffled = quips.quips.anonymize.shuffle_word(word)
+    def assertWordIsShuffledTypically(self, word, shuffled):
+        """Assert a word is shuffled, preserving first and last characters."""
         self.assertNotEqual(word, shuffled)
         self.assertEqual(word[0], shuffled[0])
         self.assertEqual(word[-1], shuffled[-1])
 
+    def test_shuffle_word_long(self):
+        """Test a reasonably long word is shuffled."""
+        word = "Enterprise"
+        shuffled = anonymize.shuffle_word(word)
+        self.assertWordIsShuffledTypically(word, shuffled)
+
     def test_shuffle_word_tiny(self):
         """Test a tiny word does not is shuffled."""
         word = "Ro"
-        shuffled = quips.quips.anonymize.shuffle_word(word)
+        shuffled = anonymize.shuffle_word(word)
         self.assertEqual(word, shuffled)
 
     def test_shuffle_word_three(self):
         """Test a three-character word is shuffled."""
         word = "Odo"
         expected = "Ood"
-        shuffled = quips.quips.anonymize.shuffle_word(word)
+        shuffled = anonymize.shuffle_word(word)
         self.assertNotEqual(word, shuffled)
         self.assertEqual(shuffled, expected)
 
@@ -30,6 +34,14 @@ class AnonymizeTest(TestCase):
         """Test a three-character word is shuffled preserving case."""
         word = "MrE"
         expected = "MeR"
-        shuffled = quips.quips.anonymize.shuffle_word(word)
+        shuffled = anonymize.shuffle_word(word)
         self.assertNotEqual(word, shuffled)
         self.assertEqual(shuffled, expected)
+
+    def test_obfuscate_name(self):
+        """Test the words are shuffled in a typical two-word name."""
+        name = "Thomas Riker"
+        obfuscated = anonymize.obfuscate_name(name)
+        self.assertNotEqual(name, obfuscated)
+        for word, obfuscated_word in zip(name.split(" "), obfuscated.split(" ")):
+            self.assertWordIsShuffledTypically(word, obfuscated_word)
