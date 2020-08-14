85ea62f4ddf8058245d992dcf2fd2dda4cc95c72
Brad Smith <bradster@infinitewarp.com>
2020-08-14 02:23:42 -0400

    reformat the slackapp view to reduce complexity and improve readability

--- quips/slackapp/views.py
+++ quips/slackapp/views.py
@@ -1,3 +1,5 @@
+"""View functionality to handle Slack's slash-command requests."""
+
 import uuid
 
 from django.http import JsonResponse
@@ -11,85 +13,136 @@ from quips.website.templatetags.quips_extras import obfuscate_name
 from quips.website.views import QuipRandomObjectBaseMixin
 
 
+def stringify_quote(quote):
+    """Format a Quip's Quote into a single line string."""
+    if quote.speaker.should_obfuscate:
+        speaker_name = obfuscate_name(str(quote.speaker))
+    else:
+        speaker_name = str(quote.speaker)
+    if quote.is_slash_me:
+        return f"{speaker_name} {quote}"
+    return f"{speaker_name}: {quote}"
+
+
+def format_response_quip(quip, quip_absolute_uri):
+    """Format a typical in-channel response containing a whole Quip."""
+    lines = [stringify_quote(quote) for quote in quip.quotes.all()]
+    if quip.context:
+        context = quip.context.replace("_", r"\_")
+        lines.append(f"_{quip.date}, {context}_")
+    else:
+        lines.append(f"_{quip.date}_")
+    lines.append(quip_absolute_uri)
+    text = "\n".join(lines)
+    response = {"response_type": "in_channel", "text": text}
+    return response
+
+
+def format_response_uuid_not_found(command_text):
+    """Format an ephemeral error when the quip UUID is not found."""
+    response = {
+        "response_type": "ephemeral",
+        "text": f"No quip found with UUID `{command_text}`.",
+    }
+    return response
+
+
+def format_response_speaker_not_found(command_text):
+    """Format an ephemeral error when the quip speaker is not found."""
+    response = {
+        "response_type": "ephemeral",
+        "text": f"No quips found involving speaker name like `{command_text}`.",
+    }
+    return response
+
+
+def format_response_generic_error(command_text):
+    """Format an ephemeral error when an unexpected error occurs."""
+    response = {
+        "response_type": "ephemeral",
+        "text": f"Unexpected error trying to find a quip for `{command_text}`.",
+    }
+    return response
+
+
+def filter_by_uuid(queryset, uuid_string):
+    """Filter a Quip-based queryset by UUID."""
+    try:
+        quip_uuid = uuid.UUID(uuid_string)
+    except ValueError:
+        raise InputNotValidUUID()
+    queryset = queryset.filter(uuid=quip_uuid)
+    if queryset.count() == 0:
+        raise QuipUuidNotFound()
+    return queryset
+
+
+def filter_by_speaker_name(queryset, speaker_name):
+    """Filter a Quip-based queryset by Quote's Speaker's name."""
+    try:
+        speaker = Speaker.objects.filter(name__icontains=speaker_name).first()
+        if speaker is None:
+            raise SpeakerNameNotFound()
+        queryset = queryset.filter(quotes__speaker=speaker)
+    except queryset.model.DoesNotExist:
+        raise SpeakerNameNotFound()
+    return queryset
+
+
 @method_decorator(csrf_exempt, name="dispatch")
 class QuipSlackView(QuipRandomObjectBaseMixin, View):
+    """View to handle a Slack app's slash-command integration."""
+
+    def get_command_text(self):
+        """Get the slash-command payload's main text component."""
+        return self.request.POST.get("text")
+
     def get_queryset(self):
+        """
+        Get the Quip queryset with additional filters applied.
+
+        If no input is given, use the default queryset.
+        If the input appears to be a UUID, filter on Quip's UUID.
+        Else, filter assuming the input is a Speaker's name (or part of one).
+        """
         queryset = super(QuipSlackView, self).get_queryset()
-        filter_input = self.request.POST.get("text")
+        filter_input = self.get_command_text()
         if filter_input is None or len(filter_input.strip()) == 0:
             # No input? No additional filter!
             return queryset
         try:
-            quip_uuid = uuid.UUID(filter_input)
-            queryset = queryset.filter(uuid=quip_uuid)
-            if queryset.count() == 0:
-                raise QuipUuidNotFound()
-        except ValueError:
-            # Not a UUID? Must be a speaker name!
-            queryset = self.filter_by_speaker_name(queryset, filter_input)
+            queryset = filter_by_uuid(queryset, filter_input)
+        except InputNotValidUUID:
+            queryset = filter_by_speaker_name(queryset, filter_input)
         return queryset
-
-    def filter_by_speaker_name(self, queryset, speaker_name):
-        try:
-            speaker = Speaker.objects.filter(name__icontains=speaker_name).first()
-            if speaker is None:
-                raise SpeakerNameNotFound()
-            queryset = queryset.filter(quotes__speaker=speaker)
-        except queryset.model.DoesNotExist:
-            raise SpeakerNameNotFound()
-        return queryset
-
-    @staticmethod
-    def _format_quote(quote):
-        if quote.speaker.should_obfuscate:
-            speaker_name = obfuscate_name(str(quote.speaker))
-        else:
-            speaker_name = str(quote.speaker)
-        if quote.is_slash_me:
-            return "{} {}".format(speaker_name, quote)
-        return "{}: {}".format(speaker_name, quote)
-
-    def _build_formatted_response(self, quip, request):
-        lines = [self._format_quote(quote) for quote in quip.quotes.all()]
-        if quip.context:
-            context = quip.context.replace("_", r"\_")
-            lines.append("_{}, {}_".format(quip.date, context))
-        else:
-            lines.append("_{}_".format(quip.date))
-        lines.append(
-            request.build_absolute_uri(reverse("website:detail", args=(quip.uuid,)))
-        )
-        text = "\n".join(lines)
-        response = {"response_type": "in_channel", "text": text}
-        return response
 
     def post(self, request, *args, **kwargs):
+        """Handle Slack's typical POST request."""
         try:
             quip = self.get_object()
+            quip_absolute_uri = request.build_absolute_uri(
+                reverse("website:detail", args=(quip.uuid,))
+            )
+            response = format_response_quip(quip, quip_absolute_uri)
         except SpeakerNameNotFound:
-            response = {
-                "response_type": "ephemeral",
-                "text": "No quips found involving speaker name like `{}`.".format(
-                    request.POST.get("text")
-                ),
-            }
-            return JsonResponse(response)
+            command_text = self.get_command_text(self)
+            response = format_response_speaker_not_found(command_text)
         except QuipUuidNotFound:
-            response = {
-                "response_type": "ephemeral",
-                "text": "No quip found with UUID `{}`.".format(
-                    request.POST.get("text")
-                ),
-            }
-            return JsonResponse(response)
-
-        response = self._build_formatted_response(quip, request)
+            command_text = self.get_command_text(self)
+            response = format_response_uuid_not_found(command_text)
+        except:  # noqa: E722
+            command_text = self.get_command_text(self)
+            response = format_response_generic_error(command_text)
         return JsonResponse(response)
 
 
 class SpeakerNameNotFound(Exception):
-    pass
+    """A quip with the given speaker name was not found."""
 
 
 class QuipUuidNotFound(Exception):
-    pass
+    """A quip with the given UUID was not found."""
+
+
+class InputNotValidUUID(Exception):
+    """The provided input string was not a valid UUID."""
