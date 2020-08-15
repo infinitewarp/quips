9715fe926874352618fa9aecee438d16b015d3a9
Brad Smith <bradster@infinitewarp.com>
2020-08-15 00:30:37 -0400

    use black for formatting

--- config/settings/test.py
+++ config/settings/test.py
@@ -8,24 +8,25 @@ Test settings
 
 from .common import *  # noqa
 
-DEBUG = env.bool('DJANGO_DEBUG', default=False)
-TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
+DEBUG = env.bool("DJANGO_DEBUG", default=False)
+TEMPLATES[0]["OPTIONS"]["debug"] = DEBUG
 
-EMAIL_HOST = 'localhost'
+EMAIL_HOST = "localhost"
 EMAIL_PORT = 1025
-EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND',
-                    default='django.core.mail.backends.console.EmailBackend')
+EMAIL_BACKEND = env(
+    "DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend"
+)
 
-SECRET_KEY = env('DJANGO_SECRET_KEY', default='test')
+SECRET_KEY = env("DJANGO_SECRET_KEY", default="test")
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION': ''
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "",
     }
 }
 
-DATABASES['default'] = env.db(default='sqlite:////db.sqlite3')
-DATABASES['default']['ATOMIC_REQUESTS'] = True
+DATABASES["default"] = env.db(default="sqlite:////db.sqlite3")
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+TEST_RUNNER = "django.test.runner.DiscoverRunner"
