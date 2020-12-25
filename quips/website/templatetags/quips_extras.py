66345ed054c907f2fd6f5a4f3b56e29e0481c65e
Brad Smith <bradster@infinitewarp.com>
2020-12-25 15:23:48 -0500

    add a few more alternate giphy embeds

--- quips/website/templatetags/quips_extras.py
+++ quips/website/templatetags/quips_extras.py
@@ -16,6 +16,12 @@ GIPHYS = [
     '<iframe src="//giphy.com/embed/baDHsD0Bk3K5a?html5=true&hideSocial=true" width="480" height="384" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',  # noqa
     '<iframe src="//giphy.com/embed/xSM46ernAUN3y?html5=true&hideSocial=true" width="480" height="384" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',  # noqa
     '<iframe src="//giphy.com/embed/jhmjR9RZYS92M?html5=true&hideSocial=true" width="480" height="384" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',  # noqa
+    '<iframe src="//giphy.com/embed/gLvBksqQE5j60Ud8R7?html5=true&hideSocial=true" width="480" height="266" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',  # noqa
+    '<iframe src="//giphy.com/embed/TJawtKM6OCKkvwCIqX?html5=true&hideSocial=true" width="480" height="354" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',  # noqa
+    '<iframe src="//giphy.com/embed/109Pz1wDYxNLmE?html5=true&hideSocial=true" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',  # noqa
+    '<iframe src="//giphy.com/embed/10p704gIAGRN7i?html5=true&hideSocial=true" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',  # noqa
+    '<iframe src="//giphy.com/embed/wHBkzKahBgFEM1S4GE?html5=true&hideSocial=true" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',  # noqa
+    '<iframe src="//giphy.com/embed/dmB5vD2t2gR8Y?html5=true&hideSocial=true" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',  # noqa
 ]
 
 
