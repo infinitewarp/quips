41344b1fa7df6adcf7697b98cb3525ddea691ec2
Brad Smith <bradster@infinitewarp.com>
2022-02-20 22:48:05 -0500

    simplify url to path

--- config/urls.py
+++ config/urls.py
@@ -19,20 +19,20 @@ if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
     urlpatterns += [
-        url(
-            r"^400/$",
+        path(
+            "400/",
             default_views.bad_request,
             kwargs={"exception": Exception("Bad Request!")},
         ),
-        url(
-            r"^403/$",
+        path(
+            "403/",
             default_views.permission_denied,
             kwargs={"exception": Exception("Permission Denied")},
         ),
-        url(
-            r"^404/$",
+        path(
+            "404/",
             default_views.page_not_found,
             kwargs={"exception": Exception("Page not Found")},
         ),
-        url(r"^500/$", default_views.server_error),
+        path("500/", default_views.server_error),
     ]
