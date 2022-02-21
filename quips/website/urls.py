b5ed72a5680efb6af11e681073bab9820dabb008
Brad Smith <bradster@infinitewarp.com>
2022-02-20 20:50:55 -0500

    simplify re_path to path

--- quips/website/urls.py
+++ quips/website/urls.py
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from django.urls import path, re_path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path("", view=views.QuipDefaultView.as_view(), name="base"),
     path("random/", view=views.QuipRandomSpeakerView.as_view(), name="random"),
-    re_path(
-        r"^clique/(?P<slug>.+)/$",
+    path(
+        "clique/<slug>/",
         view=views.QuipRandomCliqueSpeakerView.as_view(),
         name="cliquerandom",
     ),
-    re_path(
-        r"^(?P<uuid>[\d\w\-]+)/$",
-        view=views.QuipDetailView.as_view(),
-        name="detail",
-    ),
+    path("<uuid>/", view=views.QuipDetailView.as_view(), name="detail"),
 ]
