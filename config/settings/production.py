165ac611418636580edde5a43478ecfdd9d2d1f0
Brad Smith <bradster@infinitewarp.com>
2024-06-24 15:49:44 -0400

    refactor: clean up old settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Production Configurations
+Production settings overrides.
 
-- Use djangosecure
-
+- Set appropriate secrets, security, host, etc.
+- Use Redis cache.
 """
 
 from __future__ import absolute_import, unicode_literals
@@ -11,52 +11,37 @@ from __future__ import absolute_import, unicode_literals
 from .common import *  # noqa: F403
 # ruff: noqa: F405
 
-# SECRET CONFIGURATION
-# ------------------------------------------------------------------------------
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-# Raises ImproperlyConfigured exception if DJANGO_SECRET_KEY not in os.environ
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # DJANGO SECURITY
-# ------------------------------------------------------------------------------
-# See https://docs.djangoproject.com/en/2.0/topics/security/
-
+# https://docs.djangoproject.com/en/dev/topics/security/
 # set this to 60 seconds and then to 518400 when you can prove it works
 SECURE_HSTS_SECONDS = 60
 SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
     "DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True
 )
-SECURE_FRAME_DENY = env.bool("DJANGO_SECURE_FRAME_DENY", default=True)
 SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
     "DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True
 )
-SECURE_BROWSER_XSS_FILTER = True
 SESSION_COOKIE_SECURE = False
 SESSION_COOKIE_HTTPONLY = True
 SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 
-# SITE CONFIGURATION
-# ------------------------------------------------------------------------------
-# Hosts/domain names that are valid for this site
-# See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["infinitewarp.com"])
 CSRF_TRUSTED_ORIGINS = env.list(
     "DJANGO_CSRF_TRUSTED_ORIGINS", default=["https://*.infinitewarp.com"]
 )
-# END SITE CONFIGURATION
 
+# https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS += ("gunicorn",)
 
-# DATABASE CONFIGURATION
-# ------------------------------------------------------------------------------
-# Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
 DATABASES["default"] = env.db("DATABASE_URL")
 
-# CACHING
-# ------------------------------------------------------------------------------
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
@@ -72,11 +57,8 @@ CACHES = {
 }
 
 
-# LOGGING CONFIGURATION
-# ------------------------------------------------------------------------------
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#logging
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+# https://docs.djangoproject.com/en/dev/ref/settings/#logging
+# http://docs.djangoproject.com/en/dev/topics/logging
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
@@ -105,5 +87,3 @@ LOGGING = {
 
 # Custom Admin URL, use {% url 'admin:index' %}
 ADMIN_URL = env("DJANGO_ADMIN_URL")
-
-# Your production stuff: Below this line define 3rd party library settings
--- config/settings/test.py
+++ config/settings/test.py
@@ -8,17 +8,14 @@ Test settings
 from .common import *  # noqa: F403
 # ruff: noqa: F405
 
+# https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 TEMPLATES[0]["OPTIONS"]["debug"] = DEBUG
 
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY", default="test")
 
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-        "LOCATION": "",
-    }
-}
+CACHES = {"default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"}}
 
 DATABASES["default"] = env.db(default="sqlite:////db.sqlite3")
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
